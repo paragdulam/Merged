3d436def3ff2c20cb4fb092848a3e3182780d9c0
Parag Dulam <paragdulam@gmail.com>
2021-07-19 01:02:54 +0530

    Github integration done in the react app

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    client_id: "90875754817df6aa5b2c",
+    client_secret: "c77aacb74112b89373b1910a9f044a326d662e28",
+    redirect_uri: "http://localhost:3000/login",
+};  
--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 const port = 3000;
+
+
+const clientID = '90875754817df6aa5b2c';
+const clientSecret = 'c77aacb74112b89373b1910a9f044a326d662e28';
 
 app.use(bodyParser.json());
 app.use(
@@ -9,6 +14,30 @@ app.use(
     extended: true,
   })
 );
+
+app.get('/home', (req, res) => {
+
+  // The req.query object has the query params that were sent to this route.
+  const requestToken = req.query.code
+  
+  axios({
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    // Set the content type header, so that we get the response in JSON
+    headers: {
+         accept: 'application/json'
+    }
+    
+  }).then((response) => {
+    
+    const accessToken = response.data.access_token
+    console.log(response.data)
+    
+    // redirect the user to the home page, along with the access token
+    res.redirect(`/home.html?access_token=${accessToken}`)
+  })
+})
+
 
 app.get('/', (req, res) => {
   res.json({'message': 'ok'});
