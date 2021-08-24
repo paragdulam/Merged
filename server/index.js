1140005585b8c9b68994eb08cd9b513a4e656404
Parag Dulam <paragdulam@gmail.com>
2021-08-24 23:29:47 +0530

    Implemented github without db integration and deleted unwanted server.js file

--- server/index.js
+++ server/index.js
@@ -54,6 +54,6 @@ app.post("/authenticate", (req, res) => {
     });
 });
 
-require("./app/routes/user.routes.js")(app);
+// require("./app/routes/user.routes.js")(app);
 const PORT = process.env.SERVER_PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
