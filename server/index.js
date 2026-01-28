1469753fe6bb101f2de46bfe43356c0d20aac189
Parag Dulam <paragdulam@gmail.com>
2026-01-28 15:19:01 +0530

    Fixed local working

--- server/index.js
+++ server/index.js
@@ -55,5 +55,5 @@ app.post("/authenticate", (req, res) => {
 });
 
 // require("./app/routes/user.routes.js")(app);
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.SERVER_PORT || 5001;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
